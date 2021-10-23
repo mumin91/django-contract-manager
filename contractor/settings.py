f7671c218d51575a97dba26b3b502c83aa6bcb92
Muminur Rahman <r.muminur@gmail.com>
2021-10-23 12:30:46 +0600

    Fix venv

--- contractor/settings.py
+++ contractor/settings.py
@@ -96,6 +96,8 @@ else:
         }
     }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
